8e922316048ee887a55b442e6df78d5b62d9b0d5
Teerawat <my1311.dew@gmail.com>
2016-05-05 06:42:45 +0800

    Electricity calculate is finished in 3 types

--- app/src/main/java/activity/ElectricityBillFragment.java
+++ app/src/main/java/activity/ElectricityBillFragment.java
@@ -9,11 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import com.example.senoir.newpmatry1.R;
+
+import org.w3c.dom.Text;
+
 import adapter.ExpandableListAdapter;
+import billcalculate.BillCalculate;
 import model.ElectricityBillModel;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,9 +31,12 @@ public class ElectricityBillFragment extends Fragment {
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
     List<String> listDataHeader;
+    TextView totalCost;
     ArrayList<ElectricityBillModel> electricitybillList;
     HashMap<String, List<String>> listDataChild;
     private FragmentActivity myContext;
+    float electricityType = 1.31f;
+
     public ElectricityBillFragment() {
         // Required empty public constructor
     }
@@ -42,6 +51,7 @@ public class ElectricityBillFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_electricitybill, container, false);
+        totalCost = (TextView) rootView.findViewById(R.id.total_cost);
 
 
         // get the listview
@@ -56,6 +66,8 @@ public class ElectricityBillFragment extends Fragment {
 
         // setting list adapter
         expListView.setAdapter(listAdapter);
+
+
         return rootView;
     }
     private void prepareListData() {
@@ -67,11 +79,71 @@ public class ElectricityBillFragment extends Fragment {
 
         putdataToelectricityBill();
 
-        for(int start = 0; start<electricitybillList.size(); start++) {
+        double sumUnitOfLocation = 0d;
+        double sumOnPeakUnit = 0d;
+        double sumOffPeakUnit = 0d;
+
+        for(int i =0; i<electricitybillList.size(); i++){
+            if (electricityType == 1.31f || electricityType == 1.32f){
+                sumOnPeakUnit += electricitybillList.get(i).getUnitLocation();
+                sumOffPeakUnit += electricitybillList.get(i).getUnitLocation();
+            } else {
+                sumUnitOfLocation += electricitybillList.get(i).getUnitLocation();
+            }
+        }
+
+        BillCalculate bill = new BillCalculate();
+
+        DecimalFormat df = new DecimalFormat("#0.00");
+
+        double costOfLocation = 0d;
+
+        if(electricityType == 1.1f) {
+            costOfLocation = bill.getBillOfType1_1(sumUnitOfLocation);
+        } else if (electricityType == 1.2f){
+            costOfLocation = bill.getBillOfType1_2(sumUnitOfLocation);
+        } else if (electricityType == 1.31f) {
+            costOfLocation = bill.getBillOfType1_3(sumOnPeakUnit, sumOffPeakUnit, 0); // แรงดันอันที่ 1
+            sumUnitOfLocation = sumOffPeakUnit + sumOnPeakUnit;
+        } else if (electricityType == 1.32f) {
+            costOfLocation = bill.getBillOfType1_3(sumOnPeakUnit, sumOffPeakUnit, 1); // แรงดันอันที่ 2
+            sumUnitOfLocation = sumOffPeakUnit + sumOnPeakUnit;
+        }
+
+        totalCost.setText("Total: "  + df.format(costOfLocation) + " Baht/   " + df.format(sumUnitOfLocation) +" Units");
+
+        for(int start = 0; start < electricitybillList.size(); start++) {
             expandevices = new ArrayList<String>();
-            listDataHeader.add(electricitybillList.get(start).getLocationName() +":  "+ electricitybillList.get(start).getCostLocation() + "  Baht");
+            double eachCostLocation = costOfLocation * (electricitybillList.get(start).getUnitLocation()/sumUnitOfLocation);
+
+            if (electricityType == 1.31f || electricityType == 1.32f){
+                double percentOfTypeWatt = sumOffPeakUnit/sumOnPeakUnit;
+                electricitybillList.get(start).setAllCost(eachCostLocation, eachCostLocation*percentOfTypeWatt);
+            } else {
+                electricitybillList.get(start).setAllCost(eachCostLocation);
+            }
+
+            listDataHeader.add(electricitybillList.get(start).getLocationName() +":\n"+
+                    df.format(electricitybillList.get(start).getCostLocation()) + "  Baht/ " +
+                    df.format(electricitybillList.get(start).getUnitLocation()) + "  Units");
+
+//            listDataHeader.add(electricitybillList.get(start).getLocationName() +":\n"+
+//                    df.format(electricitybillList.get(start).getCostOnPeak()) + "  Baht/ " +
+//                    df.format(electricitybillList.get(start).getOnPeakLocation()) + "  Units\n" +
+//                    df.format(electricitybillList.get(start).getCostOffPeak()) + "  Baht/ " +
+//                    df.format(electricitybillList.get(start).getOffPeakLocation()) + "  Units");
+
             for (int begin = 0; begin < electricitybillList.get(start).getDevicesName().size(); begin++) {
-                expandevices.add(begin, electricitybillList.get(start).getDevicesNamestring(begin)+": "+electricitybillList.get(start).getCostDevicesint(begin)+"  Baht");
+
+                expandevices.add(begin, electricitybillList.get(start).getDevicesNamestring(begin)+":\n"+
+                        df.format(electricitybillList.get(start).getCostDevice(begin)) + "  Baht/ " +
+                        df.format(electricitybillList.get(start).getUnitDevicesdou(begin)) + "  Units");
+//                expandevices.add(begin, electricitybillList.get(start).getDevicesNamestring(begin)+":\n"+
+//                        df.format(electricitybillList.get(start).getOnPeakDevice(begin)) + "  Baht/ " +
+//                        df.format(electricitybillList.get(start).getOnPeakCostDevice(begin))+"  Units\n" +
+//                        df.format(electricitybillList.get(start).getOffPeakDevice(begin)) + "  Baht/ " +
+//                        df.format(electricitybillList.get(start).getOffPeakCostDevice(begin)) + "  Units");
+
 
             }
             listDataChild.put(listDataHeader.get(start), expandevices);
@@ -83,17 +155,35 @@ public class ElectricityBillFragment extends Fragment {
     }
     public void putdataToelectricityBill() {
 
-        for (int start = 0; start < 3; start++) {
-            ArrayList<String> devicesname = new ArrayList<String>();
-            ArrayList<Integer> costdevice = new ArrayList<Integer>();
-            for (int insides = 0; insides < 3; insides++) {
+        if (electricityType == 1.31f || electricityType == 1.32f){
+            for (int start = 1; start <= 3; start++) {
+                ArrayList<String> devicesname = new ArrayList<String>();
+                ArrayList<Double> unitOnDevice = new ArrayList<Double>();
+                ArrayList<Double> unitOffDevice = new ArrayList<Double>();
 
-                devicesname.add(insides, "Devices: " + insides);
-                costdevice.add(insides, insides * 100);
+                for (int insides = 1; insides <= 5; insides++) {
+
+                    devicesname.add("Devices: " + insides);
+                    unitOnDevice.add(insides * 100d);
+                    unitOffDevice.add(insides * 100d);
+                }
+                ElectricityBillModel electbill = new ElectricityBillModel("Location: " + start, devicesname, unitOnDevice, unitOffDevice);
+                electricitybillList.add(electbill);
+                //Log.d("TestLoop", electricitybillList.size()+"");
             }
-            ElectricityBillModel electbill = new ElectricityBillModel("Location: " + start, start * 200, devicesname, costdevice);
-            electricitybillList.add(start, electbill);
-            //Log.d("TestLoop", electricitybillList.size()+"");
+        }else{
+            for (int start = 1; start <= 3; start++) {
+                ArrayList<String> devicesname = new ArrayList<String>();
+                ArrayList<Double> unitDevice = new ArrayList<Double>();
+                for (int insides = 1; insides <= 5; insides++) {
+
+                    devicesname.add("Devices: " + insides);
+                    unitDevice.add(insides * 100d);
+                }
+                ElectricityBillModel electbill = new ElectricityBillModel("Location: " + start, devicesname, unitDevice);
+                electricitybillList.add(electbill);
+                //Log.d("TestLoop", electricitybillList.size()+"");
+            }
         }
     }
     @Override
